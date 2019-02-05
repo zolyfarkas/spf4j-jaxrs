08f0c3effe8ea25bfb1f6d2a3aabc2c8d3acbf16
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-02-05 09:59:49 -0500

    [add] a simple NONE deadline protocol

--- spf4j-jaxrs-client/src/main/java/org/spf4j/http/DeadlineProtocol.java
+++ spf4j-jaxrs-client/src/main/java/org/spf4j/http/DeadlineProtocol.java
@@ -2,6 +2,8 @@ package org.spf4j.http;
 
 import java.util.function.BiConsumer;
 import java.util.function.Function;
+import org.spf4j.base.ExecutionContexts;
+import org.spf4j.base.TimeSource;
 
 /**
  * A protocol to transmit deadline over HTTP headers.
@@ -25,5 +27,22 @@ public interface DeadlineProtocol {
    * @return the timeout in nanoseconds.
    */
   long serialize(BiConsumer<String, String> headers, long deadlineNanos);
+
+  /**
+   * A protocol, that will not read/write deadline/timeout to http headers.
+   * Useful when interacting with services that do not support this.
+   */
+  DeadlineProtocol NONE = new DeadlineProtocol() {
+    @Override
+    public long deserialize(final Function<String, String> headers, final long currentTimeNanos) {
+     return ExecutionContexts.getContextDeadlineNanos();
+    }
+
+    @Override
+    public long serialize(final BiConsumer<String, String> headers, final long deadlineNanos) {
+      return deadlineNanos - TimeSource.nanoTime();
+    }
+  };
+
 
 }
