a1d9d0d840a2e4682b558033eb5abfc924562611
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-04-05 06:28:18 -0400

    [add] prepare for release

--- spf4j-jaxrs-actuator/src/main/java/org/spf4j/actuator/logs/LogFilesResource.java
+++ spf4j-jaxrs-actuator/src/main/java/org/spf4j/actuator/logs/LogFilesResource.java
@@ -22,7 +22,6 @@ import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
