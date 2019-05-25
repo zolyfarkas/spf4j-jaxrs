cd95221e255e34d47d4e9317d0562f52267fbf66
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-05-24 21:05:25 -0400

    [add] utility resource.

---
+++ spf4j-jaxrs-server/src/main/java/org/spf4j/jaxrs/server/resources/ClassPathResource.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.jaxrs.server.resources;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.InputStream;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import org.spf4j.base.CharSequences;
+
+/**
+ *
+ * @author Zoltan Farkas
+ */
+@SuppressWarnings("checkstyle:DesignForExtension")// methods cannot be final due to interceptors
+@SuppressFBWarnings("JAXRS_ENDPOINT")
+public class ClassPathResource {
+
+  private final String cpBase;
+
+  public ClassPathResource(final String cpBase) {
+    this.cpBase = cpBase;
+  }
+
+  @Override
+  public String toString() {
+    return "ClassPathResource{" + "cpBase=" + cpBase + '}';
+  }
+
+  @GET
+  @Path("{path:.*}")
+  public Response staticResources(@PathParam("path") final String path) {
+    final InputStream resource = Thread.currentThread().getContextClassLoader()
+            .getResourceAsStream(cpBase + CharSequences.validatedFileName(path));
+    return null == resource
+        ? Response.status(404).build()
+        : Response.ok().entity(resource).build();
+  }
+
+}
--- spf4j-jaxrs-server/src/main/java/org/spf4j/jaxrs/server/resources/FilesResource.java
+++ spf4j-jaxrs-server/src/main/java/org/spf4j/jaxrs/server/resources/FilesResource.java
@@ -15,6 +15,7 @@
  */
 package org.spf4j.jaxrs.server.resources;
 
+import com.google.common.collect.Range;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -25,6 +26,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -34,10 +36,12 @@ import javax.ws.rs.core.StreamingOutput;
 import org.spf4j.base.CharSequences;
 import org.spf4j.base.avro.FileEntry;
 import org.spf4j.base.avro.FileType;
+import org.spf4j.http.HttpRange;
 import org.spf4j.io.Streams;
 
 /**
  * A naive implementation of a file tree REST "browser"
+ *
  * @author Zoltan Farkas
  */
 @SuppressWarnings("checkstyle:DesignForExtension")// methods cannot be final due to interceptors
@@ -54,7 +58,8 @@ public class FilesResource {
   @GET
   @Produces({"application/json", "application/octet-stream"})
   @SuppressFBWarnings("RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE") // try-resources gen code
-  public Response get(@PathParam("path") final List<PathSegment> path) throws IOException {
+  public Response get(@PathParam("path") final List<PathSegment> path,
+          @HeaderParam("Range") final HttpRange range) throws IOException {
     Path target = base;
     for (PathSegment part : path) {
       String p = part.getPath();
@@ -75,7 +80,21 @@ public class FilesResource {
       }
       return Response.ok(result, MediaType.APPLICATION_JSON).build();
     } else {
+
+      if (range != null && range.isByteRange()) {
+        List<Range<Long>> ranges = range.getRanges();
+        if (ranges.size() == 1) {
+          Range<Long> r = ranges.get(0);
+          return Response.status(206).entity(new StreamedFile(target, r.lowerEndpoint(), r.upperEndpoint()))
+                  .encoding(MediaType.APPLICATION_OCTET_STREAM)
+                  .header("Accept-Ranges", "bytes")
+                  .header("Content-Range", "bytes " + r.lowerEndpoint() + '-' + r.upperEndpoint() + "/*")
+                  .header("Content-Disposition", "attachment; filename=\"" + target.getFileName() + "\"")
+                  .build();
+        }
+      }
       return Response.ok(new StreamedFile(target), MediaType.APPLICATION_OCTET_STREAM)
+              .header("Accept-Ranges", "bytes")
               .header("Content-Disposition", "attachment; filename=\"" + target.getFileName() + "\"")
               .build();
     }
@@ -90,14 +109,32 @@ public class FilesResource {
 
     private final Path ft;
 
+    private final long from;
+
+    private final long to;
+
     StreamedFile(final Path ft) {
+      this(ft, 0, -1L);
+    }
+
+    StreamedFile(final Path ft, final long from, final long to) {
       this.ft = ft;
+      this.from = from;
+      this.to = to;
     }
 
     @Override
     public void write(final OutputStream output) throws IOException {
       try (BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(ft))) {
-        Streams.copy(bis, output);
+        long skip = bis.skip(from);
+        if (skip != from) {
+          throw new UnsupportedOperationException("Unable to skip " + from + " bytes, managed only " + skip);
+        }
+        if (to < 0) {
+          Streams.copy(bis, output);
+        } else {
+          Streams.copy(bis, output, 8192, to);
+        }
       }
     }
   }
