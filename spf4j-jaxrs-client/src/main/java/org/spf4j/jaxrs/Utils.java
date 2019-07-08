dcc00546e3ddcf941d4a705e240a81f187497b2a
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-07-08 11:25:17 -0400

    [add] improve default retry predicate.

--- spf4j-jaxrs-client/src/main/java/org/spf4j/jaxrs/Utils.java
+++ spf4j-jaxrs-client/src/main/java/org/spf4j/jaxrs/Utils.java
@@ -1,5 +1,6 @@
 package org.spf4j.jaxrs;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
@@ -32,49 +33,54 @@ public final class Utils {
       builder.withExceptionPartialPredicate(WebApplicationException.class, predicate, retryCount);
     }
     return builder.withExceptionPartialPredicate(WebApplicationException.class,
-                    (WebApplicationException ex, Callable<? extends Object> c) -> {
-                      Response response = ex.getResponse();
-                      String retryAfter = response.getHeaderString("Retry-After");
-                      if (retryAfter != null && !retryAfter.isEmpty()) {
-                        if (Character.isDigit(retryAfter.charAt(0))) {
-                          return RetryDecision.retry(Long.parseLong(retryAfter), TimeUnit.SECONDS, c);
-                        } else {
-                          return RetryDecision.retry(Duration.between(Instant.now(),
-                                  DateTimeFormatter.RFC_1123_DATE_TIME.parse(retryAfter,
-                                          Instant::from)).toNanos(),
-                                  TimeUnit.NANOSECONDS, c);
-                        }
-                      }
-                      String noRetry = response.getHeaderString("No-Retry");
-                      // Not standard,
-                      // but a way for the server to tell the client there is no point for the client to retry.
-                      if (noRetry == null) {
-                        return RetryDecision.abort();
-                      }
-                      int status = response.getStatus();
-                      switch (status) {
-                        case 408:
-                        case 409:
-                        case 419:
-                        case 420:
-                        case 423:
-                        case 429:
-                        case 440:
-                        case 449:
-                        case 503:
-                        case 504:
-                        case 509:
-                        case 522:
-                        case 524:
-                        case 599:
-                          return RetryDecision.retryDefault(c);
-                        default:
-                          if (status >= 400 && status < 500) {
-                            return RetryDecision.abort();
-                          }
-                      }
-                      return null;
-                    })
+            new PartialTypedExceptionRetryPredicate<Object, Callable<? extends Object>, WebApplicationException>() {
+      @Override
+      @SuppressFBWarnings("PRMC_POSSIBLY_REDUNDANT_METHOD_CALLS")
+      public RetryDecision<Object, Callable<? extends Object>> getExceptionDecision(
+              final WebApplicationException ex, final Callable<? extends Object> c) {
+        Response response = ex.getResponse();
+        String retryAfter = response.getHeaderString("Retry-After");
+        if (retryAfter != null && !retryAfter.isEmpty()) {
+          if (Character.isDigit(retryAfter.charAt(0))) {
+            return RetryDecision.retry(Long.parseLong(retryAfter), TimeUnit.SECONDS, c);
+          } else {
+            return RetryDecision.retry(Duration.between(Instant.now(),
+                    DateTimeFormatter.RFC_1123_DATE_TIME.parse(retryAfter,
+                            Instant::from)).toNanos(),
+                    TimeUnit.NANOSECONDS, c);
+          }
+        }
+        String noRetry = response.getHeaderString("No-Retry");
+        // Not standard,
+        // but a way for the server to tell the client there is no point for the client to retry.
+        if (noRetry == null) {
+          return RetryDecision.abort();
+        }
+        int status = response.getStatus();
+        switch (status) {
+          case 408:
+          case 409:
+          case 419:
+          case 420:
+          case 423:
+          case 429:
+          case 440:
+          case 449:
+          case 503:
+          case 504:
+          case 509:
+          case 522:
+          case 524:
+          case 599:
+            return RetryDecision.retryDefault(c);
+          default:
+            if (status >= 400 && status < 500) {
+              return RetryDecision.abort();
+            }
+        }
+        return null;
+      }
+    })
             .withRetryOnException(Exception.class, 2) // will retry any other exception twice.
             .build();
   }
