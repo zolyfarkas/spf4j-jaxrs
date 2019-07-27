debb874150f738296c4b462fce336d0e312560b1
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-07-27 19:33:49 -0400

    [add] grizzly utilities module

---
+++ spf4j-jaxrs-grizzly/src/main/java/org/glassfish/grizzly/servlet/FixedWebappContext.java
@@ -0,0 +1,33 @@
+
+package org.glassfish.grizzly.servlet;
+
+import javax.servlet.Servlet;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+/**
+ *
+ * @author Zoltan Farkas
+ */
+public final class FixedWebappContext extends WebappContext {
+
+  public FixedWebappContext(final String displayName) {
+    super(displayName);
+  }
+
+  public FixedWebappContext(final String displayName, final String contextPath) {
+    super(displayName, contextPath);
+  }
+
+  public FixedWebappContext(final String displayName, final String contextPath, final String basePath) {
+    super(displayName, contextPath, basePath);
+  }
+
+  @Override
+  protected Servlet createServletInstance(final ServletRegistration registration) throws Exception {
+    ServletContainer srvlet = (ServletContainer) super.createServletInstance(registration);
+    registration.servlet = srvlet;
+    return srvlet;
+  }
+
+
+}
---
+++ spf4j-jaxrs-grizzly/src/main/java/org/spf4j/grizzly/GrizzlyErrorPageGenerator.java
@@ -0,0 +1,66 @@
+package org.spf4j.grizzly;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import org.glassfish.grizzly.http.server.ErrorPageGenerator;
+import org.glassfish.grizzly.http.server.Request;
+import org.spf4j.avro.SchemaClient;
+import org.spf4j.base.Arrays;
+import org.spf4j.base.avro.Converters;
+import org.spf4j.base.avro.DebugDetail;
+import org.spf4j.base.avro.ServiceError;
+import org.spf4j.io.ByteArrayBuilder;
+import org.spf4j.jaxrs.common.providers.avro.DefaultSchemaProtocol;
+import org.spf4j.jaxrs.common.providers.avro.XJsonAvroMessageBodyWriter;
+
+/**
+ *
+ * @author Zoltan Farkas
+ */
+public final class GrizzlyErrorPageGenerator implements ErrorPageGenerator {
+
+  private final SchemaClient schemaClient;
+
+  public GrizzlyErrorPageGenerator(final SchemaClient schemaClient) {
+    this.schemaClient = schemaClient;
+  }
+
+
+
+  @Override
+  public String generate(final Request request, final int status,
+          final String reasonPhrase, final String description, final Throwable exception) {
+    ServiceError err = ServiceError.newBuilder()
+            .setCode(status)
+            .setMessage(reasonPhrase + ';' + description)
+            .setDetail(new DebugDetail("origin", Collections.EMPTY_LIST,
+                    exception != null ? Converters.convert(exception) : null, Collections.EMPTY_LIST))
+            .build();
+    ByteArrayBuilder bab = new ByteArrayBuilder(256);
+    XJsonAvroMessageBodyWriter writer = new XJsonAvroMessageBodyWriter(new DefaultSchemaProtocol(schemaClient));
+    try {
+      writer.writeTo(err, err.getClass(), err.getClass(),
+              Arrays.EMPTY_ANNOT_ARRAY, MediaType.APPLICATION_JSON_TYPE, new MultivaluedHashMap<>(2),
+              bab);
+    } catch (RuntimeException ex) {
+      if (exception != null) {
+        ex.addSuppressed(exception);
+      }
+      Logger.getLogger(reasonPhrase).log(Level.SEVERE, "RuntimeException while writing detail", ex);
+      throw ex;
+    } catch (IOException ex) {
+      if (exception != null) {
+        ex.addSuppressed(exception);
+      }
+      Logger.getLogger(reasonPhrase).log(Level.SEVERE, "Exception while writing detail", ex);
+      throw new UncheckedIOException(ex);
+    }
+    return bab.toString(StandardCharsets.UTF_8);
+  }
+}
