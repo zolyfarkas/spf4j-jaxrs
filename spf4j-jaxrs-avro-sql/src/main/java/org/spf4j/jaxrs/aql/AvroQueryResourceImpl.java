cce500d28ac276a445cac2b4c7677e9cfd9bee25
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-11-08 06:08:40 -0500

    [add] plan lookup code.

--- spf4j-jaxrs-avro-sql/src/main/java/org/spf4j/jaxrs/aql/AvroQueryResourceImpl.java
+++ spf4j-jaxrs-avro-sql/src/main/java/org/spf4j/jaxrs/aql/AvroQueryResourceImpl.java
@@ -96,7 +96,7 @@ public final class AvroQueryResourceImpl implements AvroQueryResource {
   }
 
   @Override
-  public CharSequence plan(String query) {
+  public CharSequence plan(final String query) {
     return RelOptUtil.toString(parsePlan(new StringReader(query)));
   }
 
