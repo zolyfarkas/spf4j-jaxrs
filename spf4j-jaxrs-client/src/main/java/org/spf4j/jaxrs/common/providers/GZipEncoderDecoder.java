f765b6541ebdb1a452027048c849d7ff1e542eb6
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-11-10 11:45:56 -0500

    [cleanup]

--- spf4j-jaxrs-client/src/main/java/org/spf4j/jaxrs/common/providers/GZipEncoderDecoder.java
+++ spf4j-jaxrs-client/src/main/java/org/spf4j/jaxrs/common/providers/GZipEncoderDecoder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.spf4j.jaxrs.common.providers.gp;
+package org.spf4j.jaxrs.common.providers;
 
 import java.io.IOException;
 import java.io.InputStream;
